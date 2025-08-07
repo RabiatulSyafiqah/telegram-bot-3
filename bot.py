1434bbe8a079865ba2442822003d39e40bc68ed6
RabiatulSyafiqah <rabiatulsyafiqahhh@gmail.com>
2025-08-07 11:33:57 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,11 +13,9 @@ from telegram.ext import (
 from sheet import (
     is_slot_available,
     save_booking,
-    get_alternative_times,
     is_valid_date,
     is_weekend,
-    get_available_slots,
-    sheet
+    get_available_slots
 )
 
 # Load environment variables
@@ -171,3 +169,4 @@ def index():
 if __name__ == "__main__":
 
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
