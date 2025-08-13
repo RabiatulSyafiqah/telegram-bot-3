41b452bc8d8e65baf847f835b590ccfee1d18bb1
RabiatulSyafiqah <rabiatulsyafiqahhh@gmail.com>
2025-08-13 14:28:32 +0800

    Update bot.py(update pot)

--- bot.py
+++ bot.py
@@ -193,5 +193,6 @@ def webhook_root():
 if __name__ == "__main__":
     # Initialize the application
     asyncio.run(setup_application())
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
 
